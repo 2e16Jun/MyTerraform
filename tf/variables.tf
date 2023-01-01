77a3a2e40abe9bf6e2c9750cc67c5f8876db2ca1
KJ <carder@kakao.com>
2023-01-01 20:53:45 +0900

    0.0.3, EKS.tf added, Diagram changed

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,9 +1,3 @@
-variable "instance_name" {
-  description = "Value of the Name tag for the EC2 instance"
-  type        = string
-  default     = "ExampleAppServerInstance"
-}
-
 variable "aws_region" {
   description = "AWS region to launch servers."
   default     = "ap-northeast-2"
